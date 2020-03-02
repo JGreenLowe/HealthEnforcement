4da39f05daa22adac78af2b41b2396ae81196b42
JGreenLowe <32722792+JGreenLowe@users.noreply.github.com>
2020-03-01 20:45:52 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project analyzes California healthcare enforcement actions to explore patterns in when appeals will be successful.',
+    author='Jason Green-Lowe',
+    license='MIT',
+)
